59d026f44860643e7f9b669b652f525b957aea8b
uparupa124 <repmyu2@gmail.com>
2021-01-15 20:35:59 +0000

    [finish]4-8

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -14,7 +14,7 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blog_path(blog.id)
+    redirect_to blogs_path(blog.id)
   end
   
   def edit
@@ -25,6 +25,12 @@ class BlogsController < ApplicationController
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
+  end
+  
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
   end
 private
   def blog_params
